37a473513f8815722c05297819ffb36f5627ed2a
eTRVPMAX <trvpmax9@proton.me>
2023-09-20 14:54:37 -0700

    test

--- shell.c
+++ shell.c
@@ -1,47 +1,47 @@
 #include "shell.h"
 #include <signal.h>
 
-int main(int ac, char **av, char *envp[])
+int main(int argc, char **argv, char *envv[])
 {
-    char *line = NULL, *pathcommand = NULL, *path = NULL;
-    size_t bufsize = 0;
-    ssize_t linesize = 0;
-    char **command = NULL, **paths = NULL;
-    (void)envp, (void)av;
+	char *line = NULL, *pathcommand = NULL, *path = NULL;
+	size_t bufsize = 0;
+	ssize_t linesize = 0;
+	char **commandargs = NULL, **paths = NULL;
+	(void)envv, (void)argv;
 
-    if (ac < 1)
-        return (-1);
+	if (argc < 1)
+		return (-1);
 
-    signal(SIGINT, signal_handler);
+	signals(SIGINT, signal_handler);
 
-    path = path_handler();
-    paths = tokenize(path);
+	path = path_handler();
+	paths = tokenize(path);
 
-    while (1)
-    {
-        freebuff(command);
-        free(pathcommand);
-        prompt();
-        linesize = getline(&line, &bufsize, stdin);
-        if (linesize < 0)
-            break;
-        info.count++;
-        if (line[linesize - 1] == '\n')
-            line[linesize - 1] = '\0';
-        command = tokenize(line);
-        if (!command || !(*command) || !(**command))
-            continue;
-        if (checkbuiltin(command, line))
-            continue;
-        pathcommand = checkpath(paths, command[0]);
-        if (!pathcommand)
-            perror(av[0]);
-        else
-            execute(pathcommand, command);
-    }
+	while (1)
+	{
+		freebuff(commandargs);
+		free(pathcommand);
+		prompt();
+		linesize = getline(&line, &bufsize, stdin);
+		if (linesize < 0)
+			break;
+		info.count++;
+		if (line[linesize - 1] == '\n')
+			line[linesize - 1] = '\0';
+		commandargs = tokenize(line);
+		if (!commandargs || !(*commandargs) || !(**commandargs))
+			continue;
+		if (checkbuiltin(commandargs, line))
+			continue;
+		pathcommand = checkpath(paths, commandargs[0]);
+		if (!pathcommand)
+			perror(argv[0]);
+		else
+			execute(pathcommand, commandargs);
+	}
 
-    freebuff(command);
-    freebuff(paths);
-    free(line);
-    return (0);
+	freebuff(commandargs);
+	freebuff(paths);
+	free(line);
+	return (0);
 }
