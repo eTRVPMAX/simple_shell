abb86f557221f8d52c4f5243274e7f0504099cb7
eTRVPMAX <trvpmax9@proton.me>
2023-09-20 15:39:06 -0700

    test

--- shell.c
+++ shell.c
@@ -1,51 +1,59 @@
 #include "shell.h"
 #include <signal.h>
 
-/**
- * main - carries out the read, execute, then print output loop
- * @ac: argument count
- * @av: argument vector
- * @envp: environment vector
- *
- * Return: 0
- */
-int main(int argc, char **argv, char *envv[])
-{
-    char *line = NULL, *pathcommand = NULL, *path = NULL;
+void readAndExecuteCommands(char **paths, char *av0) {
+    char *line = NULL;
     size_t bufsize = 0;
     ssize_t linesize = 0;
-    char **command = NULL, **paths = NULL;
-    (void)envv, (void)argv;
+    char **command = NULL;
+    char *pathcommand = NULL;
+    
+    while (1)
+    {
+        prompt();
+        linesize = getline(&line, &bufsize, stdin);
+        if (linesize < 0)
+            break;
+        info.count++;
+        if (line[linesize - 1] == '\n')
+            line[linesize - 1] = '\0';
+        command = tokenize(line);
+        if (!command || !(*command) || !(**command))
+            continue;
+        if (checkbuiltin(command, line))
+            continue;
+        pathcommand = checkpath(paths, command[0]);
+        if (!pathcommand)
+            perror(av0);
+        else
+            execute(pathcommand, command);
+    }
 
-    if (argc < 1)
+    if (linesize < 0 && flags.interactive)
+        write(STDERR_FILENO, "\n", 1);
+
+    freebuff(command);
+    free(line);
+}
+
+int main(int ac, char **av, char *envp[])
+{
+    char *path = NULL;
+    char **paths = NULL;
+
+    (void)envp, (void)av;
+
+    if (ac < 1)
         return (-1);
 
     signal(SIGINT, signal_handler);
 
     path = path_handler();
-    paths = tokenize(path_handler());
+    paths = tokenize(path);
 
-    while (1)
-    {
-        freebuff(command);
-        free(pathcommand);
-        prompt();
-        linesize = getline(&line, &bufsize, stdin);
-        if (linesize < 0) break;
-        info.count++;
-        if (line[linesize - 1] == '\n') line[linesize - 1] = '\0';
-        command = tokenize(line);
-        if (!command || !(*command) || !(**command)) 
-			continue;
-        if (checkbuiltin(command, line)) 
-			continue;
-        pathcommand = checkpath(paths, command[0]);
-        if (!pathcommand) perror(argv[0]);
-        else execute(pathcommand, command);
-    }
-    if (linesize < 0 && flags.interactive) write(STDERR_FILENO, "\n", 1);
-    freebuff(command);
+    readAndExecuteCommands(paths, av[0]);
+
     freebuff(paths);
-    free(line);
+    free(path);
     return (0);
 }
