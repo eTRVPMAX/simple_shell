a54a4ca874461b2e2d253acc94dc16603ffb7ab7
eTRVPMAX <trvpmax9@proton.me>
2023-09-20 16:41:41 -0700

    test

--- shell.c
+++ shell.c
@@ -1,15 +1,21 @@
 #include "shell.h"
 #include <signal.h>
 
-void handlecommands(char **paths, char *av0) {
-    char *line = NULL;
+int main(int ac, char **av, char *envp[])
+{
+    char *line = NULL, *pathcommand = NULL, *path = NULL;
     size_t bufsize = 0;
     ssize_t linesize = 0;
-    char **command = NULL;
-    char *pathcommand = NULL;
-    
+    char **command = NULL, **paths = NULL;
+    (void)envp, (void)av;
+    if (ac < 1)
+        return (-1);
+    signal(SIGINT, signal_handler);
     while (1)
     {
+        freebuff(command);
+        freebuff(paths);
+        free(pathcommand);
         prompt();
         linesize = getline(&line, &bufsize, stdin);
         if (linesize < 0)
@@ -18,42 +24,39 @@ void handlecommands(char **paths, char *av0) {
         if (line[linesize - 1] == '\n')
             line[linesize - 1] = '\0';
         command = tokenize(line);
-        if (!command || !(*command) || !(**command))
-            continue;
-        if (checkbuiltin(command, line))
-            continue;
+        if (command == NULL || *command == NULL || **command == '\0') continue;
+        if (checkbuiltin(command, line)) continue;
+        path = path_handler();
+        paths = tokenize(path);
         pathcommand = checkpath(paths, command[0]);
-        if (!pathcommand)
-            perror(av0);
+        if (!pathcommand) 
+        {
+            perror(av[0]);
+        }
         else
-            execute(pathcommand, command);
+        {
+            switch (fork())
+            {
+                case -1:
+                    perror(av[0]);
+                    break;
+                case 0:
+                    if (execve(pathcommand, command, environ) == -1)
+                    {
+                        perror(av[0]);
+                        freebuff(command);
+                        free(line);
+                        exit(98);
+                    }
+                    break;
+                default:
+                    wait(NULL);
+                    break;
+            }
+        }
     }
-
     if (linesize < 0 && flags.interactive)
         write(STDERR_FILENO, "\n", 1);
-
-    freebuff(command);
     free(line);
-}
-
-int main(int ac, char **av, char *envp[])
-{
-    char *path = NULL;
-    char **paths = NULL;
-
-    (void)envp, (void)av;
-
-    if (ac < 1)
-        return (-1);
-
-    signal(SIGINT, signal_handler);
-
-    path = path_handler();
-    paths = tokenize(path);
-
-    handlecommands(paths, av[0]);
-
-    freebuff(paths);
-    free(path);
     return (0);
 }
