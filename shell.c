c383530cce59e0aec47f01920e287bd196d558de
eTRVPMAX <trvpmax9@proton.me>
2023-09-20 14:42:56 -0700

    just for test

--- shell.c
+++ shell.c
@@ -1,50 +1,47 @@
 #include "shell.h"
-/**
-* main - carries out the read, execute then print output loop
-* @ac: argument count
-* @av: argument vector
-* @envp: environment vector
-*
-* Return: 0
-*/
+#include <signal.h>
 
 int main(int ac, char **av, char *envp[])
 {
-	char *line = NULL, *pathcommand = NULL, *path = NULL;
-	size_t bufsize = 0;
-	ssize_t linesize = 0;
-	char **command = NULL, **paths = NULL;
-	(void)envp, (void)av;
-	if (ac < 1)
-		return (-1);
-	signal(SIGINT, signal_handler);
-	while (1)
-	{
-		freebuff(command);
-		freebuff(paths);
-		free(pathcommand);
-		prompt();
-		linesize = getline(&line, &bufsize, stdin);
-		if (linesize < 0)
-			break;
-		info.count++;
-		if (line[linesize - 1] == '\n')
-			line[linesize - 1] = '\0';
-		command = tokenize(line);
-		if (command == NULL || *command == NULL || **command == '\0')
-			continue;
-		if (checkbuiltin(command, line))
-			continue;
-		path = path_handler();
-		paths = tokenize(path);
-		pathcommand = checkpath(paths, command[0]);
-		if (!pathcommand)
-			perror(av[0]);
-		else
-			execute(pathcommand, command);
-	}
-	if (linesize < 0 && flags.interactive)
-		write(STDERR_FILENO, "\n", 1);
-	free(line);
-	return (0);
+    char *line = NULL, *pathcommand = NULL, *path = NULL;
+    size_t bufsize = 0;
+    ssize_t linesize = 0;
+    char **command = NULL, **paths = NULL;
+    (void)envp, (void)av;
+
+    if (ac < 1)
+        return (-1);
+
+    signal(SIGINT, signal_handler);
+
+    path = path_handler();
+    paths = tokenize(path);
+
+    while (1)
+    {
+        freebuff(command);
+        free(pathcommand);
+        prompt();
+        linesize = getline(&line, &bufsize, stdin);
+        if (linesize < 0)
+            break;
+        info.count++;
+        if (line[linesize - 1] == '\n')
+            line[linesize - 1] = '\0';
+        command = tokenize(line);
+        if (!command || !(*command) || !(**command))
+            continue;
+        if (checkbuiltin(command, line))
+            continue;
+        pathcommand = checkpath(paths, command[0]);
+        if (!pathcommand)
+            perror(av[0]);
+        else
+            execute(pathcommand, command);
+    }
+
+    freebuff(command);
+    freebuff(paths);
+    free(line);
+    return (0);
 }
