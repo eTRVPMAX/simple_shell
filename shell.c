e623dc4de53842bf8561a1759fa83093f0c6740c
eTRVPMAX <trvpmax9@proton.me>
2023-09-20 18:09:46 -0700

    init

--- shell.c
+++ shell.c
@@ -1,70 +1,61 @@
 #include "shell.h"
 #include <signal.h>
 
-/**
-* main - carries out the read, execute then print output loop
-* @argc: argument count
-* @argv: argument vector
-* @envp: environment vector
-*
-* Return: 0
-*/
-int main(int ac, char **av, char *envp[])
+int main(int argc, char **argv, char *envp[])
 {
 	char *line = NULL, *pathcommand = NULL, *path = NULL;
 	size_t bufsize = 0;
 	ssize_t linesize = 0;
 	char **command = NULL, **paths = NULL;
-	(void)envp, (void)av;
-	if (ac < 1)
+
+	(void)envp, (void)argv;
+
+	if (argc < 1)
 		return (-1);
+
 	signal(SIGINT, signal_handler);
+
 	while (1)
 	{
 		freebuff(command);
 		freebuff(paths);
 		free(pathcommand);
 		prompt();
+
 		linesize = getline(&line, &bufsize, stdin);
 		if (linesize < 0)
 			break;
+
 		info.count++;
+
 		if (line[linesize - 1] == '\n')
 			line[linesize - 1] = '\0';
+
 		command = tokenize(line);
-		if (command == NULL || *command == NULL || **command == '\0') continue;
-		if (checkbuiltin(command, line)) continue;
-		path = path_handler();
-		paths = tokenize(path);
-		pathcommand = checkpath(paths, command[0]);
-		if (!pathcommand)
+		if (command == NULL || *command == NULL || **command == '\0')
+			continue;
+
+		switch (checkbuiltin(command, line))
 		{
-			perror(av[0]);
-		}
-		else
-		{
-			switch (fork())
-			{
-				case -1:
-					perror(av[0]);
-					break;
-				case 0:
-					if (execve(pathcommand, command, environ) == -1)
-					{
-						perror(av[0]);
-						freebuff(command);
-						free(line);
-						exit(98);
-					}
-					break;
-				default:
-					wait(NULL);
-					break;
-			}
+			case 1:
+				continue;
+			case 2:
+				path = path_handler();
+				paths = tokenize(path);
+				pathcommand = checkpath(paths, command[0]);
+				if (!pathcommand)
+					perror(argv[0]);
+				else
+					execute(pathcommand, command);
+				break;
+			default:
+				break;
 		}
 	}
+
 	if (linesize < 0 && flags.interactive)
 		write(STDERR_FILENO, "\n", 1);
+
 	free(line);
 	return (0);
 }
